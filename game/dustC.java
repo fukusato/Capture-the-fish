14798c5d9a522fc762983a0052e1ccbccf93889a
Ryota-Iida <12303@hakoadate.kosen-ac.jp>
2017-12-15 10:17:44 +0900

    Dが魚とゴミを追加

---
+++ game/dustC.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class dustC here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class dustC extends Actor
+{
+    /**
+     * Act - do whatever the dustC wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
