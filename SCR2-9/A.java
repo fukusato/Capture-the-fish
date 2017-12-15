c61d7e40b801e5fd64160ae879c63f2614496f88
fukusato <monnsuta_meito@yahoo.co.jp>
2017-12-15 09:36:40 +0900

    新シナリオ作成

--- SCR2-9/A.java
+++ SCR2-9/A.java
@@ -14,6 +14,9 @@ public class A extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(0);
+        move(1);
+}// Add your action code here.
     }    
 }
