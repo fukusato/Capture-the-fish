7412fcf90ba925eb5c1b761951f300b56a1b4f7e
Mekab <xexixsxexix.mth@gmail.com>
2017-12-15 09:47:49 +0900

    hoge

--- SCR2-9/A.java
+++ SCR2-9/A.java
@@ -14,23 +14,21 @@ public class A extends Actor
      */
     public void act() 
     {
-<<<<<<< HEAD
+
         if( Greenfoot.isKeyDown( "up" ) ){
         setRotation(0);
         move(1);
 }// Add your action code here.
-=======
+
         if( Greenfoot.isKeyDown( "down" ) ){
         setRotation(90);
         move(1);
 }
-<<<<<<< HEAD
+
         if( Greenfoot.isKeyDown( "right" ) ){
         setRotation(0);
         move(1);
 }
-=======
->>>>>>> dd4b8d274ec6fc5fbfe7bf88fd9c1f6bb6db1538
->>>>>>> af079cb9b54ba2d807c898bcf4c36021dbd8a5da
+
     }    
 }
