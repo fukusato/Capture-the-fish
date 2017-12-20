7968885cbb7f5ee428b3d457d3bdd8cf8b094f85
fukusato <monnsuta_meito@yahoo.co.jp>
2017-12-20 10:00:40 +0900

    Aが追加

--- game/Fish.java
+++ game/Fish.java
@@ -12,8 +12,15 @@ public class Fish extends Actor
      * Act - do whatever the Fish wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int fish;
+    public void Fish()
+    {
+        fish=0;
+    }
     public void act() 
     {
         // Add your action code here.
+        fish++;
+        getWorld().showText("score="+fish, 300, 50);
     }    
 }
