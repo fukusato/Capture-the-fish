c0cfe6d76776864f7576e28d23a5744d1822ab84
fukusato <monnsuta_meito@yahoo.co.jp>
2018-01-26 09:39:17 +0900

    A

--- game/fishA.java
+++ game/fishA.java
@@ -15,7 +15,25 @@ public class fishA extends Fish
     public void act() 
     {
         setRotation(90);
-        move(1);// Add your action code here.
+        move(1);// Add your action code here   
+
+    int y1=0;
+    //for(int i=0;y==800;i++){
+        y1=getY();
+        getWorld().showText("y="+y1, 100, 200);
+    //}
+        if(y1==799){
+            getWorld().removeObject( this );
+             int xmax = 400;
+         int xmin = 100;
+         int x = xmin + (int)(Math.random()*((xmax-xmin)+1));
+         
+         int ymax = 400;
+         int ymin = 0;
+         int y = ymin + (int)(Math.random()*((ymax-ymin)+1));
+         
+         getWorld().addObject( new fishA(),x , y );
         
-    }    
+    }
+}
 }
